7297f45113633a80f7d36e7d0707be1e9716471f
Abdullah Hamid <hafizabdullahqurashi1@gmail.com>
2023-12-23 18:51:43 +0500

    asdasddsj

--- src/Components/Pages/Aboutpage.jsx
+++ src/Components/Pages/Aboutpage.jsx
@@ -5,7 +5,7 @@ import Navbar from '../Home/Navbar';
 import Footer from '../Home/Footer';
 const AboutPage = () => {
     return (
-        <div className='about-parent'>
+        <div className='about-parent' style={{padding:"160px 0 0 0"}}>
             <Navbar/>
             <div className="about-grid">
                 <div className='about-text'>
@@ -19,7 +19,7 @@ const AboutPage = () => {
                             <button></button>
                         </div>
                         <div className='about-detail-text'>
-                            <p>Dynamic ReactJS and Node.js developer with a formidable two-year track record. Expertise in crafting high-impact, responsive interfaces and robust server-side solutions. Demonstrated ability to innovate and streamline development processes. Ready to elevate your projects with my seasoned skills.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam reiciendis, non id explicabo, quasi eveniet, temporibus sed dolorum dicta cupiditate nobis cumque suscipit. Omnis delectus reprehenderit quasi quisquam, dolorem, corporis amet atque libero minus nisi aliquid asperiores? Dolorem suscipit eum officia praesentium sunt eaque voluptatem error hic? Obcaecati officiis reiciendis quibusdam totam illum? Repudiandae rem sapiente error sed quis. Dicta aspernatur repellat aliquid vitae corporis eum repudiandae quis dignissimos cumque quas. Maxime, esse soluta! Accusamus aspernatur perspiciatis repellat cum tenetur, dolores consequuntur nihil a rem in officia aut? Dolorum fugit nemo at deleniti, sed illo possimus maxime dolore vero odio.</p>
                             <a href="Abdullah Hamid Cv.pdf" download='Abdullah Hamid Cv.pdf'>  <button className='download'>Download Cv <FiDownload /></button></a>
                         </div>
                     </div>
