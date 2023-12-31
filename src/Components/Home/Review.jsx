7dbcb56e550157d6345cfffd6bf792f6fc5d0cf6
Abdullah Hamid <hafizabdullahqurashi1@gmail.com>
2023-12-31 22:40:49 +0500

    aasdads

--- src/Components/Home/Review.jsx
+++ src/Components/Home/Review.jsx
@@ -13,7 +13,7 @@ import 'swiper/css/pagination';
 import './Scss/Review.scss';
 
 // import required modules
-import { FreeMode } from 'swiper/modules';
+
 import { Reviewdata } from '../Data';
 
 export default function Review() {
@@ -30,7 +30,7 @@ export default function Review() {
         pagination={{
           clickable: true,
         }}
-        modules={[FreeMode]}
+        modules={[]}
         className="mySwiper"
       >
         {
